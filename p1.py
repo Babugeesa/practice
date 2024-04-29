b5ae68976d8b45329c3d923a4b959aee16b49f7e
Babugeesa <139646321+Babugeesa@users.noreply.github.com>
2024-04-29 02:40:59 -0700

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,17 @@
+import math
+number=int(input("Enter numbers"))
+angle1=math.pi/4
+angle2=math.pi/6
+result=math.pow(2,3)
+angle_in_degrees=math.degrees(angle1)
+square_root=math.sqrt(number)
+cosine=math.cos(angle1)
+sine=math.sin(angle2)
+print("The square root of",number,",is",square_root)
+print("The cosine of",angle1,"is",cosine)
+print("The sine of",angle2,"is",sine)
+print(result)
+print(angle1,"radians is equal to",angle_in_degrees,"degrees")
+print("math.fabs(-13.1):",math.fabs(-13))
+print("math.fabs(101.96);",math.fabs(101.96))
+print("math.pi",math.pi)
